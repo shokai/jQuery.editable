8f03d51d8ea95983a6d7ffeef40015bf563b3e6e
Sho Hashimoto <hashimoto@shokai.org>
2012-03-29 17:16:28 +0900

    fix indent

--- jquery.editable.js
+++ jquery.editable.js
@@ -3,37 +3,30 @@
 // https://github.com/shokai/jQuery.editable
 
 (function($){
-     $.fn.editable = function(event, callback){
-         if(typeof callback != 'function') callback = function(arg){};
+    $.fn.editable = function(event, callback){
+        if(typeof callback != 'function') callback = function(arg){};
 
-         var that = this;
-         var edit_start = function(){
-             that.unbind(event);
-             var old_value = that.html().replace(/^\s+/,'').replace(/\s+$/,'');
-             var input = $('<input>').
-                 attr('id','editable_'+(new Date()*1)).
-                 addClass('editable').
-                 val( old_value );
-             var finish = function(){
-                 var res = input.val().replace(/^\s+/,'').replace(/\s+$/,'');
-                 that.html(res);
-                 that.bind(event, edit_start);
-                 callback({value : res, target : that, oldValue : old_value});
-             }
-             
-             input.blur(finish);
-             input.keydown(
-                 function(e){
-                     if(e.keyCode == 13){
-                         finish();
-                     }
-                 }
-             );
+        var that = this;
+        var edit_start = function(){
+            that.unbind(event);
+            var old_value = that.html().replace(/^\s+/,'').replace(/\s+$/,'');
+            var input = $('<input>').val(old_value).attr('id','editable_'+(new Date()*1)).addClass('editable');
+            var finish = function(){
+                var res = input.val().replace(/^\s+/,'').replace(/\s+$/,'');
+                that.html(res);
+                that.bind(event, edit_start);
+                callback({value : res, target : that, oldValue : old_value});
+            }
 
-             that.html(input);
-             input.focus();
-         };
-         
-         that.bind(event, edit_start);
-     };
- })(jQuery);
+            input.blur(finish);
+            input.keydown(function(e){
+                if(e.keyCode == 13) finish();
+            });
+
+            that.html(input);
+            input.focus();
+        };
+
+        that.bind(event, edit_start);
+    };
+})(jQuery);
