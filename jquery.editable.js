c7f057c4d5c507434350cca3f389e643a3f3c534
Sho Hashimoto <hashimoto@shokai.org>
2012-03-31 15:30:13 +0900

    font-size 100% textinput in editable mode

--- jquery.editable.js
+++ jquery.editable.js
@@ -22,8 +22,8 @@
             if(trigger != target) trigger.hide();
             var old_value = target.text().replace(/^\s+/,'').replace(/\s+$/,'');
             var input = $('<input>').val(old_value).
-                width(target.width()).
-                attr('id','editable_'+(new Date()*1)).
+                width(target.width()*1.1).css('font-size','100%').
+                css('margin',0).attr('id','editable_'+(new Date()*1)).
                 addClass('editable');
             var finish = function(){
                 var res = input.val().replace(/^\s+/,'').replace(/\s+$/,'');
