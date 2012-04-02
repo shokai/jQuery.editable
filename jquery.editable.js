e093cb165f837786fe56efb6718546a6f17e0d89
Sho Hashimoto <hashimoto@shokai.org>
2012-04-02 15:20:52 +0900

    fix input width

--- jquery.editable.js
+++ jquery.editable.js
@@ -22,7 +22,7 @@
             if(trigger != target) trigger.hide();
             var old_value = target.text().replace(/^\s+/,'').replace(/\s+$/,'');
             var input = $('<input>').val(old_value).
-                width(target.width()*1.1).css('font-size','100%').
+                width(target.width()+target.height()).css('font-size','100%').
                 css('margin',0).attr('id','editable_'+(new Date()*1)).
                 addClass('editable');
             var finish = function(){
