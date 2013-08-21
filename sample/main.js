9969275906d78424ff3af7c267c666a53dd9d202
Sho Hashimoto <hashimoto@shokai.org>
2013-08-22 08:12:25 +0900

    change indent level 4 -> 2

--- sample/main.js
+++ sample/main.js
@@ -1,46 +1,46 @@
 $(function(){
-    $('h1').editable('mouseover');
-    
-    // double click
-    $('.name .editable').editable('dblclick', function(e){
-        alert(e.target.selector + ' : ' + e.value);
-        console.log(e.value);
-    });
-    
-    // single click
-    $('.city .editable').editable('click', function(e){
-        console.log(e.value);
-    });
-    
-    // click hold
-    $('.website .editable').editable('clickhold', function(e){
-        var a_tag = $('<a>').attr('href', e.value).text(e.value);
-        $('.website .editable').html(a_tag);
-        alert(e.value);
-        console.log(e.value);
-    });
+  $('h1').editable('mouseover');
 
-    // validation
-    $('.zip .editable').editable('click', function(e){
-        if(e.value.match(/^\d{3}\-\d{4}$/)){
-            alert(e.target.selector + ' : ' + e.value);
-        }
-        else{
-            $('.zip .editable').html(e.old_value);
-            alert(e.value + ' is not valid zip-code');
-        }
-    });
+  // double click
+  $('.name .editable').editable('dblclick', function(e){
+    alert(e.target.selector + ' : ' + e.value);
+    console.log(e.value);
+  });
 
-    // edit button
-    var option = {trigger : $('.age .btn_edit'), action : 'click'};
-    $('.age .editable').editable(option, function(e){
-        if(!e.value.match(/^\d+$/)){
-            $('.age .editable').html(e.old_value);
-            alert(e.value + ' is not valid age');
-        }
-        else{
-            alert('save : '+e.value);
-        }
-    });
+  // single click
+  $('.city .editable').editable('click', function(e){
+    console.log(e.value);
+  });
+
+  // click hold
+  $('.website .editable').editable('clickhold', function(e){
+    var a_tag = $('<a>').attr('href', e.value).text(e.value);
+    $('.website .editable').html(a_tag);
+    alert(e.value);
+    console.log(e.value);
+  });
+
+  // validation
+  $('.zip .editable').editable('click', function(e){
+    if(e.value.match(/^\d{3}\-\d{4}$/)){
+      alert(e.target.selector + ' : ' + e.value);
+    }
+    else{
+      $('.zip .editable').html(e.old_value);
+      alert(e.value + ' is not valid zip-code');
+    }
+  });
+
+  // edit button
+  var option = {trigger : $('.age .btn_edit'), action : 'click'};
+  $('.age .editable').editable(option, function(e){
+    if(!e.value.match(/^\d+$/)){
+      $('.age .editable').html(e.old_value);
+      alert(e.value + ' is not valid age');
+    }
+    else{
+      alert('save : '+e.value);
+    }
+  });
 
 });
--- src/jquery.editable.js
+++ src/jquery.editable.js
@@ -1,61 +1,62 @@
 (function($){
-    $.fn.editable = function(event, callback){
-        if(typeof callback != 'function') callback = function(arg){};
-        if(typeof event == "string"){
-            var trigger = this;
-            var action = event;
-        }
-        else{
-            var trigger = event.trigger;
-            var action = event.action;
-        }
+  $.fn.editable = function(event, callback){
 
-        var target = this;
-        var edit = {};
+    if(typeof callback != 'function') callback = function(arg){};
+    if(typeof event == "string"){
+      var trigger = this;
+      var action = event;
+    }
+    else{
+      var trigger = event.trigger;
+      var action = event.action;
+    }
 
-        edit.start = function(e){
-            trigger.unbind(action == 'clickhold' ? 'mousedown' : action);
-            if(trigger != target) trigger.hide();
-            var old_value = target.text().replace(/^\s+/,'').replace(/\s+$/,'');
-            var input = $('<input>').val(old_value).
-                width(target.width()+target.height()).css('font-size','100%').
-                css('margin',0).attr('id','editable_'+(new Date()*1)).
-                addClass('editable');
-            var finish = function(){
-                var res = input.val().replace(/^\s+/,'').replace(/\s+$/,'');
-                target.text(res);
-                callback({value : res, target : target, old_value : old_value});
-                edit.register();
-                if(trigger != target) trigger.show();
-            };
+    var target = this;
+    var edit = {};
 
-            input.blur(finish);
-            input.keydown(function(e){
-                if(e.keyCode == 13) finish();
-            });
-
-            target.html(input);
-            input.focus();
-        };
-
-        edit.register = function(){
-            if(action == 'clickhold'){
-                var tid = null;
-                trigger.bind('mousedown', function(e){
-                    tid = setTimeout(function(){
-                        edit.start(e);
-                    }, 500);
-                });
-                trigger.bind('mouseup mouseout', function(e){
-                    clearTimeout(tid);
-                });
-            }
-            else{
-                trigger.bind(action, edit.start);
-            }
-        };
+    edit.start = function(e){
+      trigger.unbind(action == 'clickhold' ? 'mousedown' : action);
+      if(trigger != target) trigger.hide();
+      var old_value = target.text().replace(/^\s+/,'').replace(/\s+$/,'');
+      var input = $('<input>').val(old_value).
+        width(target.width()+target.height()).css('font-size','100%').
+        css('margin',0).attr('id','editable_'+(new Date()*1)).
+        addClass('editable');
+      var finish = function(){
+        var res = input.val().replace(/^\s+/,'').replace(/\s+$/,'');
+        target.text(res);
+        callback({value : res, target : target, old_value : old_value});
         edit.register();
+        if(trigger != target) trigger.show();
+      };
 
-        return this;
+      input.blur(finish);
+      input.keydown(function(e){
+        if(e.keyCode == 13) finish();
+      });
+
+      target.html(input);
+      input.focus();
     };
+
+    edit.register = function(){
+      if(action == 'clickhold'){
+        var tid = null;
+        trigger.bind('mousedown', function(e){
+          tid = setTimeout(function(){
+            edit.start(e);
+          }, 500);
+        });
+        trigger.bind('mouseup mouseout', function(e){
+          clearTimeout(tid);
+        });
+      }
+      else{
+        trigger.bind(action, edit.start);
+      }
+    };
+    edit.register();
+
+    return this;
+  };
 })(jQuery);
